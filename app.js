ef29380895335262cbbd77a5c9011905cae50036
Leandro Machado <83804283+LeoMac99@users.noreply.github.com>
2022-08-19 22:50:44 +1200

    Changed app.js

--- app.js
+++ app.js
@@ -20,17 +20,17 @@ app.get('/', (req, res) => res.send('Hello world!'));
 
 app.use('/api/books', books);
 
-if(process.env.NODE_ENV === "production"){
+// if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, '/my-app/build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'my-app', 'build', 'index.html'));
     })
-}else{
-    app.get('/', (req, res)=>{
-        res.send('Api running');
-    })
-}
+// }else{
+//     app.get('/', (req, res)=>{
+//         res.send('Api running');
+//     })
+//}
 
 const port = process.env.PORT || 8082;
 
