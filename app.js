5fcbaf9f35fbdba0235f3b0c58e05b96f674c188
Leandro Machado <83804283+LeoMac99@users.noreply.github.com>
2022-08-19 21:02:45 +1200

    Final deployment

--- app.js
+++ app.js
@@ -9,19 +9,27 @@ const books = require('./routes/api/books');
 
 const app = express();
 
-// Connect Database
 connectDB();
 
-// cors
 app.use(cors({ origin: true, credentials: true }));
 
-// Init Middleware
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-// use Routes
 app.use('/api/books', books);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '/my-app/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+}else{
+    app.get('/', (req, res)=>{
+        res.send('Api running');
+    })
+}
 
 const port = process.env.PORT || 8082;
 
